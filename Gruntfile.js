09c06b2a471163d6eb8d241791d9748c5c5c5738
Marco Au <marco.au.321@gmail.com>
2014-06-11 11:01:12 -0700

    Fixed typos in karma config file.
    Fixed typos in karma config file.
    
    Fixed typos in karma config file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -396,8 +396,9 @@ module.exports = function (grunt) {
     // Test settings
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
+        configFile: 'test/karma.conf.js',
+        singleRun: false,
+        browsers: ['PhantomJS'],
       }
     },
 
