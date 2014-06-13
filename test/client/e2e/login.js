a5febdc5ebc5a78819482d730f4b996b5ecd1fba
Marco Au <marco.au.321@gmail.com>
2014-06-13 09:46:10 -0700

    Wrote e2e tests for login.

--- test/client/e2e/login.js
+++ test/client/e2e/login.js
@@ -1,12 +1,26 @@
-describe('home', function(){
+describe('login', function(){
 
-  it('should login & redirect to home for valid user', function(){
+  beforeEach(function(){
     browser.get('http://0.0.0.0:9000/login');
-    element(by.model('user.email')).sendKeys('test@test.com');
-    element(by.model('user.password')).sendKeys('test');
-    element(by.css('.form')).submit();
-    browser.getLocationAbsUrl().then(function(url){
-      expect(url).toBe('http://0.0.0.0:9000/');
+  });
+
+  describe('successful login', function(){
+    beforeEach(function(){
+      element(by.model('user.email')).sendKeys('test@test.com');
+      element(by.model('user.password')).sendKeys('test');
+      element(by.css('.form')).submit();
+    });
+
+    it('should login & redirect to home for valid user', function(){
+      browser.getLocationAbsUrl().then(function(url){
+        expect(url).toBe('http://0.0.0.0:9000/');
+      });
+    });
+
+    it('should deny login attempt for wrong email/password combo', function(){
+      browser.getLocationAbsUrl().then(function(url){
+        expect(url).toBe('http://0.0.0.0:9000/login');
+      });
     });
   });
 
