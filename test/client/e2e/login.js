081cbcc610c8ef9ef63f9d592d18b2dd394cba5a
Marco Au <marco.au.321@gmail.com>
2014-06-13 10:37:14 -0700

    Updated e2e login test.

--- test/client/e2e/login.js
+++ test/client/e2e/login.js
@@ -1,27 +1,19 @@
-describe('login', function(){
+describe('home', function(){
 
-  beforeEach(function(){
+  describe('successful login with correct email/password combo', function(){
     browser.get('http://0.0.0.0:9000/login');
-  });
-
-  describe('successful login', function(){
-    beforeEach(function(){
-      element(by.model('user.email')).sendKeys('test@test.com');
-      element(by.model('user.password')).sendKeys('test');
-      element(by.css('.form')).submit();
-    });
-
+    element(by.model('user.email')).sendKeys('test@test.com');
+    element(by.model('user.password')).sendKeys('test');
+    element(by.css('.form')).submit();
+  
     it('should login & redirect to home for valid user', function(){
       browser.getLocationAbsUrl().then(function(url){
         expect(url).toBe('http://0.0.0.0:9000/');
       });
     });
 
-    it('should deny login attempt for wrong email/password combo', function(){
-      browser.getLocationAbsUrl().then(function(url){
-        expect(url).toBe('http://0.0.0.0:9000/login');
-      });
-    });
   });
+
+  describe('failed login', function(){});
 
 });
