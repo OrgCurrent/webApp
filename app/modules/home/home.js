d5d55823cec6eb04f129c5ca8b4beab7b37632cd
Omkar Vedpathak <omkarv@gmail.com>
2014-06-26 19:12:23 -0700

    Removed rating button reference for executives

--- app/modules/home/home.js
+++ app/modules/home/home.js
@@ -2,4 +2,12 @@
 
 angular.module('happyMeterApp')
   .controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.isEmployee = function() {
+      console.log($scope.currentUser);
+      return $scope.currentUser ? $scope.currentUser.role === 'employee' : false;
+    };
+
+    $scope.isExecutive = function() {
+      return $scope.currentUser ? $scope.currentUser.role === 'executive' : false;
+    };
   }]);
